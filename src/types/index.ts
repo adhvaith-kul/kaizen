19f38ef740c7a4cb8432cb98534abbf68ff4e80d
AdhKul <adhvaith9@gmail.com>
2026-03-06 00:48:52 +0530

    advanced settings while creating groups

--- src/types/index.ts
+++ src/types/index.ts
@@ -7,11 +7,17 @@ export interface User {
   password?: string;
 }
 
+export interface GroupSettings {
+  allowedCategories: Category[];
+  habitsPerCategory: Partial<Record<Category, number>>;
+}
+
 export interface Group {
   id: string;
   name: string;
   code: string;
   createdBy: string; // userId
+  settings?: GroupSettings;
 }
 
 export interface GroupMember {
